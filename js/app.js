30ad669b89e5309bc719ffa838f73236c78142f8
retamozok <matiasagustin.1823@gmail.com>
2021-10-19 11:25:37 -0300

    clase 19/10

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#boton-bazar").click(function(){
+        $("#electro,#cocina,#bazar").show()
+       $("#electro,#cocina").toggle()
+    }); 
+
+    $("#boton-cocina").click(function(){
+        $("#electro,#cocina,#bazar").show()
+        $("#electro,#bazar").toggle()
+     }); 
+
+     $("#boton-electro").click(function(){
+        $("#electro,#cocina,#bazar").show()
+        $("#cocina,#bazar").toggle()
+     }); 
+
+     $("#boton-todo").click(function(){
+        $("#electro,#cocina,#bazar").show()
+     }); 
+
+     $("#boton-tema").click(function(){
+     $("#tema-alterno").toggleClass(".tema-alterno") 
+    }); 
+
+
+});
+
